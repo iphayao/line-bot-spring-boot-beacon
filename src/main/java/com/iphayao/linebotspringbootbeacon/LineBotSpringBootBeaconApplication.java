80fee0f78cd408fcd3dce686432ecf8a15a913d2
iphayao <phayaob@gmail.com>
2018-12-15 22:02:22 +0700

    Initial project

---
+++ src/main/java/com/iphayao/linebotspringbootbeacon/LineBotSpringBootBeaconApplication.java
@@ -0,0 +1,14 @@
+package com.iphayao.linebotspringbootbeacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineBotSpringBootBeaconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineBotSpringBootBeaconApplication.class, args);
+	}
+
+}
+
